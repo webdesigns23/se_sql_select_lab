b57ac03b867409f87f4ad07b77fedf442fa8dba2
Laura Berge <lauraeberge@gmail.com>
2025-04-23 11:17:51 -0500

    convert jupyter to plain py file

---
+++ main.py
@@ -0,0 +1,39 @@
+# STEP 1A
+# Import SQL Library and Pandas
+
+# STEP 1B
+# Connect to the database
+conn = None
+
+
+# STEP 2
+# Replace None with your code
+df_first_five = None
+
+# STEP 3
+# Replace None with your code
+df_five_reverse = None
+
+# STEP 4
+# Replace None with your code
+df_alias = None
+
+# STEP 5
+# Replace None with your code
+df_executive = None
+
+# STEP 6
+# Replace None with your code
+df_name_length = None
+
+# STEP 7
+# Replace None with your code
+df_short_title = None
+
+# STEP 8
+# Replace None with your code
+sum_total_price = None
+
+# STEP 9
+# Replace None with your code
+df_day_month_year = None
